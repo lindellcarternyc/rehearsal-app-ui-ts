d7c1562663c4c937019bb74ebc5dcfb5cc803c4e
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-11 18:56:54 -0500

    Removed boilerplate from App

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <div className='App-header'>
-          <h2>Welcome to React</h2>
-        </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        Hello
       </div>
     )
   }

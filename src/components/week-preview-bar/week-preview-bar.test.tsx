6087218e3cd6617d178f93df6a19d0245e8b6494
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-12 04:57:44 -0500

    Updated tests

--- src/components/week-preview-bar/week-preview-bar.test.tsx
+++ src/components/week-preview-bar/week-preview-bar.test.tsx
@@ -3,9 +3,17 @@ import * as ReactDOM from 'react-dom'
 
 import WeekPreviewBar from './week-preview-bar'
 
+const cb = jest.fn()
 describe('WeekPreviewBar', () => {
   it('should render without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<WeekPreviewBar />, div)
+    ReactDOM.render(
+      <WeekPreviewBar 
+        backCallback={cb}
+        forwardCallback={cb}
+        dayCallback={cb}
+      />, 
+      div
+    )
   })
 })

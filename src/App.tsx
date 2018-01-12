7899cf08a359f47723a4be35b17aa06c61c3c00a
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-11 21:30:57 -0500

    Added WeekPreviewBar and callbacks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react'
 import './App.css'
 
+import WeekPreviewBar from './components/week-preview-bar/week-preview-bar'
+
 class App extends React.Component {
+  backCallback  = (): void => {
+    console.dir('BACK')
+  }
+  forwardCallback = (): void => {
+    console.dir('FORWARD')
+  }
+  dayCallback = (num: number): void => {
+    console.dir(`DAY: ${num}`)
+  }
   render() {
     return (
       <div className='App'>
-        Hello
+        <WeekPreviewBar 
+          backCallback={this.backCallback}
+          forwardCallback={this.forwardCallback}
+          dayCallback={this.dayCallback}
+        />
       </div>
     )
   }

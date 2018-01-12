2d51694fa154ede437f729b90d3d79f4048ded11
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-11 19:09:49 -0500

    Loaded semantic-ui-css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+
+import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 
 ReactDOM.render(

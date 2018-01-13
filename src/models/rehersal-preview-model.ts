0ecf91a38123bcf32d9bcd2a9dcbeb9cc75a6c7f
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-13 12:40:00 -0500

    Added start time and end time to rehearsal preview model

--- src/models/rehersal-preview-model.ts
+++ src/models/rehersal-preview-model.ts
@@ -1,8 +1,13 @@
 import Day from './day'
+import * as moment from 'moment'
 export default class RehearsalPreviewModel {
   readonly day: Day
+  readonly startTime: moment.Moment
+  readonly endTime: moment.Moment
 
-  constructor(day: Day) {
+  constructor(day: Day, startTime: moment.Moment, endTime: moment.Moment) {
     this.day = day
+    this.startTime = startTime
+    this.endTime = endTime
   }
 }

265794320850f90374d381c8fdc5b8a6d99238ec
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-12 21:40:04 -0500

    Added test for week prop

--- src/components/schedule-preview/schedule-preview.test.tsx
+++ src/components/schedule-preview/schedule-preview.test.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+// import { shallow } from 'enzyme'
 
 import SchedulePreview from './schedule-preview'
 import { SchedulePreviewProps } from './schedule-preview'
+import Week from '../../models/week'
+import * as moment from 'moment'
 
 describe('SchedulePreview', () => {
   it('should render without crashing', () => {
     const div = document.createElement('div')
 
+    const props: SchedulePreviewProps = {
+      week: new Week(moment())
+    }
     ReactDOM.render(
-      <SchedulePreview />,
+      <SchedulePreview {...props}/>,
       div
     )
-  })
-
-  it('should have a default week prop that is this week', () => {
-    expect(SchedulePreviewProps).not.toBeUndefined()
   })
 })

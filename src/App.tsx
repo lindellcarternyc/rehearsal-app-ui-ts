51fb17868ca619c0b89be93dceb1728719dce236
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-12 22:03:23 -0500

    Removed dummy <p>

--- src/App.tsx
+++ src/App.tsx
@@ -44,8 +44,6 @@ class App extends React.Component<Props, State> {
           dayCallback={this.dayCallback}
           week={week}
         />
-        <p>{week.start}</p>
-        <p>{week.end}</p>
         <SchedulePreview week={week}/>
       </div>
     )

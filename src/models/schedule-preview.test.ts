41ff3272310a4fa28e305bb705bbc16bb6862d0f
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-12 22:00:50 -0500

    Created and renamed test file

--- src/models/schedule-preview.test.ts
+++ src/models/schedule-preview.test.ts
@@ -1,8 +1,13 @@
 import SchedulePreview from './schedule-preview'
+import * as moment from 'moment'
+import Week from './week'
 
 describe('SchedulePreview', () => {
   it('must have a week and rehearsal list', () => {
-    const preview = new SchedulePreview()
+    const m = moment('1-12-18', 'MM-DD-YY')
+    const w = new Week(m)
+  
+    const preview = new SchedulePreview(w)
     expect(preview).not.toBeUndefined()
   })
 })

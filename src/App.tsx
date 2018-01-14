a1cb285fbf6870e7cf56528a5eab1d30599238c6
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-14 17:58:09 -0500

    Changed App to use new WeekViewComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,55 @@
 import * as React from 'react'
 import './App.css'
-
-import { WeekPreviewBar } from './components'
-import Week from './models/week'
-import Day from './models/day'
-import * as moment from 'moment'
-import SchedulePreviewModel from './models/schedule-preview'
-import RehearsalPreviewModel from './models/rehersal-preview-model'
-
-const mockDay = new Day(moment('1-12-2018', 'MM-DD-YYYY'))
-const mockStart1 = moment('1-12-2018 9:00', 'MM-DD-YYYY HH:mm')
-const mockEnd1 = moment('1-12-2018 12:00', 'MM-DD-YYYY HH:mm')
-const mockRehearsal = new RehearsalPreviewModel(mockDay, mockStart1, mockEnd1)
-
-const mockDay2 = new Day(moment('1-14-2018', 'MM-DD-YYYY'))
-const mockStart2 = moment('1-14-2018 9:00', 'MM-DD-YYYY HH:mm')
-const mockEnd2 = moment('1-14-2018 12:00', 'MM-DD-YYYY HH:mm')
-const mockStart22 = moment('1-14-2018 15:00', 'MM-DD-YYYY HH:mm')
-const mockEnd22 = moment('1-14-2018 19:00', 'MM-DD-YYYY HH:mm')
-
-const mockDay3 = new Day(moment('1-15-2018', 'MM-DD-YYYY'))
-const mockStart3 = moment('1-15-2018 8:00', 'MM-DD-YYYY HH:mm')
-const mockEnd3 = moment('1-15-2018 14:00', 'MM-DD-YYYY HH:mm')
-
-const mockReharsals2 = [
-  new RehearsalPreviewModel(mockDay2, mockStart2, mockEnd2), 
-  new RehearsalPreviewModel(mockDay2, mockStart22, mockEnd22), 
-  new RehearsalPreviewModel(mockDay3, mockStart3, mockEnd3)
-]
-
-import SchedulePreview from './components/schedule-preview/schedule-preview'
+import { WeekViewComponent } from './components/week-view/week-view-component';
 
 interface Props {
 
 }
 
 interface State {
-  week: Week
-  schedulePreview: SchedulePreviewModel
+  // weekview: boolean
 }
+
+const MOCK: {date: string, times?: string[]}[] = [
+  {
+    date: 'Sun, January 14'
+  },
+  {
+    date: 'Mon, January 15'
+  },
+  {
+    date: 'Tue, January 16',
+    times: ['19:00 - 22:00']
+  },
+  {
+    date: 'Wed, January 17'
+  },
+  {
+    date: 'Thu, January 18'
+  },
+  {
+    date: 'Fri, January 19'
+  },
+  {
+    date: 'Sat, January 20',
+    times: [
+      '13:00 - 14:30',
+      '14:30 - 16:00',
+      '16:00 - 18:00'
+    ]
+  }
+]
 
 class App extends React.Component<Props, State> {
   constructor() {
     super({})
-    const week = new Week(mockDay.moment)
-    const preview = new SchedulePreviewModel(week, [mockRehearsal])
     this.state = {
-      week,
-      schedulePreview: preview
+
     }
-  }
-
-  update(week: Week) {
-    let rehearsalList: RehearsalPreviewModel[] = []
-    if (week.start === 'Jan 7') {
-      rehearsalList.push(mockRehearsal)
-    } else if (week.start === 'Jan 14') {
-      rehearsalList = mockReharsals2
-    }
-    const schedulePreview = new SchedulePreviewModel(week, rehearsalList)
-    this.setState({week, schedulePreview})
-  }
-
-  backCallback  = (): void => {
-    const { week } = this.state
-    this.update(week.previousWeek())
-  }
-
-  forwardCallback = (): void => {
-    const { week } = this.state
-    this.update(week.nextWeek())
-  }
-  dayCallback = (num: number): void => {
-    console.dir(`DAY: ${num}`)
   }
   
   render() {
-    const { week, schedulePreview } = this.state
-    return (
-      <div className='App'>
-        <WeekPreviewBar 
-          backCallback={this.backCallback}
-          forwardCallback={this.forwardCallback}
-          dayCallback={this.dayCallback}
-          week={week}
-        />
-        <SchedulePreview model={schedulePreview}/>
-      </div>
-    )
+    return <WeekViewComponent days={MOCK}/>
   }
 }
 

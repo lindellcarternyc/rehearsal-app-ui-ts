3e762076e50ee22b9f553f0a8f54e3b754776c60
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-14 21:19:45 -0500

    Created mock weeks

---
+++ src/mocks/data/week2.ts
@@ -0,0 +1,34 @@
+import { WeekViewComponentProps } from '../../components/week-view/week-view-component'
+
+const WEEK_2: WeekViewComponentProps = {
+  days: [
+    {
+      date: 'Sun, January 21'
+    },
+    {
+      date: 'Mon, January 22'
+    },
+    {
+      date: 'Tue, January 23',
+      times: ['19:00 - 20:30', '20:30 - 10:00']
+    },
+    {
+      date: 'Wed, January 24'
+    },
+    {
+      date: 'Thu, January 25'
+    },
+    {
+      date: 'Fri, January 26'
+    },
+    {
+      date: 'Sat, January 27',
+      times: [
+        '13:00 - 15:30',
+        '16:00 - 18:00'
+      ]
+    }
+  ]
+}
+
+export default WEEK_2

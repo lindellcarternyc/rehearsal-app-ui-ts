5e4e17244b94fdc43d1d0c98c857dab0b5ae4838
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-14 22:20:07 -0500

    Added new mock

---
+++ src/mocks/data/week0.ts
@@ -0,0 +1,29 @@
+import { WeekViewComponentData } from '../../components/week-view/week-view-component'
+
+const WEEK_0: WeekViewComponentData = {
+  days: [
+    {
+      date: 'Sun, January 7'
+    },
+    {
+      date: 'Mon, January 8'
+    },
+    {
+      date: 'Tue, January 9'
+    },
+    {
+      date: 'Wed, January 10'
+    },
+    {
+      date: 'Thu, January 11'
+    },
+    {
+      date: 'Fri, January 12'
+    },
+    {
+      date: 'Sat, January 13'
+    }
+  ]
+}
+
+export default WEEK_0
--- src/mocks/data/weeks.ts
+++ src/mocks/data/weeks.ts
@@ -1,10 +1,11 @@
 import { WeekViewComponentData } from '../../components/week-view/week-view-component'
 
+import WEEK_0 from './week0'
 import WEEK_1 from './week1'
 import WEEK_2 from './week2'
 
 const WEEKS: WeekViewComponentData[] = [
-  WEEK_1, WEEK_2
+  WEEK_0, WEEK_1, WEEK_2
 ]
 
 export default WEEKS

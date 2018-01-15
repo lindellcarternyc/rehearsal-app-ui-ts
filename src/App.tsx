9ce9daef0d1359e5d25896b12f27b6ff6974c391
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-14 21:45:37 -0500

    Added mock switchting

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,43 @@ interface Props {
 }
 
 interface State {
-  // weekview: boolean
+  week: number
 }
 
 import WEEK_1 from './mocks/data/week1'
+import WEEK_2 from './mocks/data/week2'
 
 class App extends React.Component<Props, State> {
   constructor() {
     super({})
     this.state = {
+      week: 1
+    }
+  }
 
+  next = () => {
+    if (this.state.week === 1) {
+      this.setState({week: 2})
+    }
+  }
+
+  previous = () => {
+    if (this.state.week === 2) {
+      this.setState({week: 1})
     }
   }
   
   render() {
-    return <WeekViewComponent days={WEEK_1.days}/>
+    const week = this.state.week === 1 ? 
+      WEEK_1 : WEEK_2
+    const days = week.days
+    return (
+      <WeekViewComponent 
+        days={days}
+        previous={() => console.dir('p')}
+        next={() => {console.dir('n')}}
+      />
+    )
   }
 }
 

0acabb1ab3571837025bb1dd0c632dbea980bf0a
Lindell Carter <lindellcarternyc@gmail.com>
2018-01-22 19:43:26 -0500

    Updated rehearsal-view

--- src/components/rehearsal-view/rehearsal-view.test.tsx
+++ src/components/rehearsal-view/rehearsal-view.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import * as renderer from 'react-test-renderer'
 
-import RehearsalView, { RehearsalViewProps } from './rehearsal-view-component'
+import RehearsalView, { RehearsalViewProps } from './rehearsal-view'
 
 const dismissRehearsal = jest.fn()
 
